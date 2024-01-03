7958c2487e6bf2eba0a1395d909962572e3260e1
Mahan Rahimi <155547692+mahanmr02@users.noreply.github.com>
2024-01-03 22:04:33 +0330

    Update footer.php

--- template/app/layouts/footer.php
+++ template/app/layouts/footer.php
@@ -22,7 +22,6 @@
                     <?php }?>
                 </div>
                 <div class="col-lg-4 col-md-6 single-footer-widget">
-                    <h4>ایسنتاگرام</h4>
                     <ul class="instafeed d-flex flex-wrap">
                         <li><img src="img/i1.jpg" alt=""></li>
                         <li><img src="img/i2.jpg" alt=""></li>
@@ -36,14 +35,6 @@
                 </div>
             </div>
             <div class="footer-bottom row align-items-center">
-                <p class="footer-text m-0 col-lg-8 col-md-12">
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;
-                    <script>
-                        document.write(new Date().getFullYear());
-                    </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                </p>
                 <div class="col-lg-4 col-md-12 footer-social">
                     <a href="https://www.instagram.com/mahanmr02?igsh=MTB0ZjUxbWxwaWpiaQ=="><i class="fa fa-instagram"></i></a>
                     <a href="https://twitter.com/AnnoYin_O_Yang?s=09"><i class="fa fa-twitter"></i></a>
